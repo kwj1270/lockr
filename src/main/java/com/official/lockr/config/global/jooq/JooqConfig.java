4690ef642f3321578bed53059ead4a2d27e63492
woozi <kwj1270@naver.com>
2025-08-25 23:24:28 +0900

    feat(kwj1270): jOOQ 인프라 및 컴파일 설정 추가
    - docker-compose.yaml 추가
    - MySQL 설정 추가
    - jOOQ 설정 추가

---
+++ src/main/java/com/official/lockr/config/global/jooq/JooqConfig.java
@@ -0,0 +1,20 @@
+package com.official.lockr.config.global.jooq;
+
+import org.jooq.conf.ExecuteWithoutWhere;
+import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JooqConfig {
+    @Bean
+    public DefaultConfigurationCustomizer jooqDefaultConfigurationCustomizer() {
+        return c -> {
+            c.set(PerformanceListener::new);
+            c.settings()
+                    .withExecuteDeleteWithoutWhere(ExecuteWithoutWhere.THROW)
+                    .withExecuteUpdateWithoutWhere(ExecuteWithoutWhere.THROW)
+                    .withRenderSchema(false);
+        };
+    }
+}
