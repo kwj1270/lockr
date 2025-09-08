49896af349e038b1bbcb8b87e5d86e1dd2a8c80f
woozi <kwj1270@naver.com>
2025-09-08 23:39:37 +0900

    chore:(kwj1270): 테스트 컨테이너와 인수 테스트 환경 설정
    - jOOQ 테스트 컨테이너 + Flyway 방식으로 동작 변경
    - 테스트 환경 테스트 컨테이너 + Flyway 방식으로 동작하여 인수 테스트 환경 구축

---
+++ src/test/java/com/official/lockr/MySQLTestContainerExtension.java
@@ -0,0 +1,32 @@
+package com.official.lockr;
+
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.Extension;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.testcontainers.containers.MySQLContainer;
+
+public class MySQLTestContainerExtension implements Extension, BeforeAllCallback {
+
+    private static final String DOCKER_IMAGE = "mysql:8.0.29";
+    private static final MySQLContainer MYSQL_CONTAINER = new MySQLContainer<>(DOCKER_IMAGE)
+            .withDatabaseName("lockr")
+            .withUsername("root")
+            .withPassword("1234");
+
+
+    @Override
+    public void beforeAll(final ExtensionContext extensionContext) {
+        if (MYSQL_CONTAINER.isRunning()) {
+            return;
+        }
+        MYSQL_CONTAINER.start();
+        System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl());
+        System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
+        System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
+        System.setProperty("spring.datasource.driver-class-name", MYSQL_CONTAINER.getDriverClassName());
+        System.setProperty("spring.flyway.url", MYSQL_CONTAINER.getJdbcUrl());
+        System.setProperty("spring.flyway.user", MYSQL_CONTAINER.getUsername());
+        System.setProperty("spring.flyway.password", MYSQL_CONTAINER.getPassword());
+    }
+}
+
